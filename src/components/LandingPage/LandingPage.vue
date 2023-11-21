539711ec6af61ac99954161bc7d69f94de497c2e
mgrybyk <mykola.grybyk@gmail.com>
2023-11-21 21:06:42 +0100

    add facebook button

--- src/components/LandingPage/LandingPage.vue
+++ src/components/LandingPage/LandingPage.vue
@@ -2,7 +2,7 @@
 
 <template>
   <h1>Ксения Крутенко - Энерготерапевт</h1>
-  <div class="flex sm:flex-wrap-reverse md:flex-nowrap">
+  <div class="flex flex-wrap-reverse md:flex-nowrap">
     <div class="about mr-5 p-2">
       <section class="mb-10">
         <h2>Я это Я</h2>
@@ -22,7 +22,7 @@
             style="background-color: #0088cc"
           >
             <span class="i-fa6-brands:telegram"></span>
-            <span>@blessing936energy</span>
+            <span>t.me/blessing936energy</span>
           </a>
 
           <!-- Youtube -->
@@ -63,22 +63,37 @@
             style="background-color: #c13584"
           >
             <span class="i-fa6-brands:instagram"></span>
-            <span>@ksu_cosmicgirl</span>
+            <span>ksu_cosmicgirl</span>
+          </a>
+
+          <!-- Facebook Button -->
+          <a
+            tabindex="0"
+            href="https://www.facebook.com/KseniyaKrutenko"
+            target="_blank"
+            rel="noopener"
+            class="social-button mb-2 mr-2 inline-block rounded-3xl px-2 py-1 leading-10 text-white shadow-md transition-all duration-150 ease-in-out focus:shadow-xl hover:shadow-xl hover:ring-1 hover:ring-inset"
+            style="background-color: #1877f2"
+          >
+            <span class="i-fa6-brands:facebook"></span>
+            <span>KseniyaKrutenko</span>
           </a>
         </p>
       </section>
     </div>
-    <img src="/img/kseniia.png" class="profile-img p2" alt="Ксения Крутенко" />
+    <img
+      src="/img/kseniia.png"
+      class="profile-img p2 md:-mt-4"
+      alt="Ксения Крутенко"
+      width="177"
+      height="300"
+    />
   </div>
 </template>
 
 <style scoped lang="css">
 div.flex {
   font-family: Arial, Helvetica, sans-serif;
-}
-img.profile-img {
-  margin-top: -2em;
-  height: 300px;
 }
 .social-button {
   white-space: nowrap;
